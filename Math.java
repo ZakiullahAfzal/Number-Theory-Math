08a1b9bb0cfaffd3c3d1b00ef236fa34d41bd056
ZakiullahAfzal <101314613+ZakiullahAfzal@users.noreply.github.com>
2025-11-23 08:08:17 +0000

    Update Math.java

--- Math.java
+++ Math.java
@@ -33,20 +33,18 @@ final class Math{
      */
     private static final int[] primes = {2,3,5,7};
 
-    public static boolean isPrime(int n){
-        for (int i : primes){
-            if (n == i) return true;
-            if (n % i == 0) return false;
-        }
+    public static boolean isPrime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
 
-        int i = 6;
-        while (i * i <= n){
-            if (n % (i + 5) == 0 || n % (i + 7) == 0) return false;
-            i += 6;
-        }
-
-        return true;
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
     }
+
+    return true;
+}
 
     //===========================================================//
 
